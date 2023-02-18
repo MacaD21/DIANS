8c0f9730e7a2a9078219d9b4ca522452251640e7
Marija Dimitrova <66965063+MacaD21@users.noreply.github.com>
2023-02-18 19:29:34 +0100

    Delete Domashna4/FilijaliController/FilijaliIBankomati1 directory

--- Domashna4/FilijaliController/FilijaliIBankomati1/src/main/java/com/example/filijaliibankomati/model/InvalidUserCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.filijaliibankomati.model;
-
-public class InvalidUserCredentialsException extends RuntimeException{
-
-    public InvalidUserCredentialsException() {
-        super("Invalid user credentials exception");
-    }
-}
--- Domashna4/FilijaliController/FilijaliIBankomati1/src/main/java/com/example/filijaliibankomati/model/UsernameExistsException.java
+++
@@ -1,6 +0,0 @@
-package com.example.filijaliibankomati.model;
-
-public class UsernameExistsException extends RuntimeException{
-    public UsernameExistsException(String username) {
-        super(String.format("User with username: %d already exists", username));    }
-}
